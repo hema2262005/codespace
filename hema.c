9acb658ddb4af56c6e72ede1ec4ef1f4ec72ecfc
hema2262005 <143392425+hema2262005@users.noreply.github.com>
2023-09-07 21:35:12 +0000

    Thu, Sep 7, 2023, 9:35 PM +00:00

--- hema.c
+++ hema.c
@@ -4,7 +4,15 @@
 int main(void){
 string s = get_string("Before: ");
 printf("after: ");
-for(i)
+for(int i=0 ; i<strlen(s);i++){
+    if(s[i]>='a' && s[i]<='z'){
+        printf("%c",s[i]-32);
+    }
+
+    else{
+        printf("%c",s[i]);
+    }
+}
 
 
 }
