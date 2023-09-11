fa2771e12b14d8d38b52cfda1dfe784c3d130648
hema2262005 <143392425+hema2262005@users.noreply.github.com>
2023-09-11 22:07:56 +0000

    Mon, Sep 11, 2023, 10:07 PM +00:00

--- hema.c
+++ hema.c
@@ -1,16 +1,4 @@
-#include<stdio.h>
 #include<cs50.h>
+#include<stdio.h>
 #include<string.h>
-int main(void){
-string names[] = {"ali","hema"};
-string numbers[] = {"011153","010256"};
-for(int i=0; i<2; i++){
-if(strcmp(names[i],"ali")==0){
-    printf("found%s\n",numbers[i]);
-    return 0;
-}
 
-}
-printf("not found\n");
-return 1;
-}
