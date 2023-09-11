e306ddf961d39c94e51186a34059157e43def2cf
hema2262005 <143392425+hema2262005@users.noreply.github.com>
2023-09-11 22:24:11 +0000

    Mon, Sep 11, 2023, 10:24 PM +00:00

--- hema.c
+++ hema.c
@@ -6,10 +6,15 @@ typedef struct{
     string numbers;
 } pb;
 int main(void){
-
-    
+pb people[2];
+people[0].names="ali";
+people[0].numbers="0115033";
+people[1].names="david";
+people[1].numbers="01521635";
     for(int i = 0 ; i < 2 ; i++){
-if()
+if(strcmp(people[i].names,"ali")==0){printf("found %s/n",people[i].numbers);
+return 0;
+}
 
     }
 }
