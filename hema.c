ea72e103beb78ef432e1d7b240039b2eebd6351b
hema2262005 <143392425+hema2262005@users.noreply.github.com>
2023-09-11 00:15:13 +0000

    Mon, Sep 11, 2023, 12:15 AM +00:00

--- hema.c
+++ hema.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<cs50.h>
 int main(void){
-int num[] = {4,5,84,5,8,1,0};
+int num[] = {4,5,7,5,8,1,0};
 for( int i=0; i<7; i++){
     if(num[i]==0){
         printf("found\n");
