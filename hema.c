6228c9d526b4ec56ea905fcf419f413c82adeb3e
hema2262005 <143392425+hema2262005@users.noreply.github.com>
2023-09-11 00:11:41 +0000

    Mon, Sep 11, 2023, 12:11 AM +00:00

---
+++ hema.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<cs50.h>
+int main(void){
+int num[] = {4,5,84,5,8,1,0};
+for( int i=0; i<7; i++){
+    if(num[i]==0){
+        printf("found\n");
+        return 0;
+    }
+}
+printf("not found\n");
+return 1;
+
+}
