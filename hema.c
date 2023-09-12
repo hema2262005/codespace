a7435cdf1baf4e0b5a13379629c6eac4c9a29be4
hema2262005 <143392425+hema2262005@users.noreply.github.com>
2023-09-12 00:48:44 +0000

    Tue, Sep 12, 2023, 12:48 AM +00:00

--- hema.c
+++ hema.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 #include<cs50.h>
-
+void draw(int n);
 int main(void){
-int height=get_height("height: ");
+int height=get_int("height: ");
 draw(height);
 void draw(int n){
 for(int i=0; i<n; i++){
     for(int j=0; j<n; j++)
     printf("#");
-}
+
+}printf("\n");
 
 }
 
