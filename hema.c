41574978eaa134e27a55ba4fe831e27003bf182c
hema2262005 <143392425+hema2262005@users.noreply.github.com>
2023-09-22 08:18:27 +0000

    Fri, Sep 22, 2023, 8:18 AM +00:00

--- hema.c
+++ hema.c
@@ -3,5 +3,9 @@
 
 int main(void){
 
-    
+    char* i =get_string("i: ");
+    char* j =get_string("j: ");
+    if(strcmp(i,j)==0){
+        printf("same\n");
+    }else{printf("not the same bitch\n";)}
 }
