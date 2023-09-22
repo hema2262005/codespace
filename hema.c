5a869bbdffa948fcf236fde6a0ae506f19e94843
hema2262005 <143392425+hema2262005@users.noreply.github.com>
2023-09-22 08:20:31 +0000

    Fri, Sep 22, 2023, 8:20 AM +00:00

--- hema.c
+++ hema.c
@@ -1,11 +1,11 @@
 #include<cs50.h>
 #include<stdio.h>
-
+#include<string.h>
 int main(void){
 
     char* i =get_string("i: ");
     char* j =get_string("j: ");
     if(strcmp(i,j)==0){
         printf("same\n");
-    }else{printf("not the same bitch\n";)}
+    }else{printf("not the same bitch\n");}
 }
